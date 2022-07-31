f13b950135daa372a7409e8edf838ae7facf0f04
srushtimane27 <srushtimane31@gmail.com>
2022-07-31 11:12:12 +0530

    Function for checking the user registration return true if matches otherwise throws exception

--- src/main/java/com/Bridgelabz/UserRegistration.java
+++ src/main/java/com/Bridgelabz/UserRegistration.java
@@ -7,20 +7,20 @@ public class UserRegistration {
     /**
      * Function for Checking the FirstName
      * @return True if matches regex pattern otherwise throws exception
-     * @throws ValidationOfException
+     * @throws Exception
      */
-    public boolean checkFirstname(String fName) throws ValidationOfException {
+    public boolean checkFirstname(String fName) throws Exception {
         boolean check = Pattern.matches("[A-Z]+[a-z]{2,}", fName);
         if (check)
             return true;
         else
-            throw new ValidationOfException("Invalid First name");
+            throw new Exception("Invalid First name");
     }
 
     /**
      * Function for Checking the LastName
      * @return True if matches regex pattern otherwise throws exception
-     * @throws ValidationOfException
+     * @throws Exception
      */
     public boolean checkLastName(String Lname) throws ValidationOfException {
         boolean check = Pattern.matches("[A-Z]+[a-z]{2,}", Lname);
@@ -33,7 +33,7 @@ public class UserRegistration {
     /**
      * Function for Checking the EmailID
      * @return True if matches regex pattern otherwise throws exception
-     * @throws ValidationOfException
+     * @throws Exception
      */
     public boolean checkEmailId(String EmailId) throws ValidationOfException {
         boolean check = Pattern.matches("^[a-z0-9.+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", EmailId);
@@ -46,7 +46,7 @@ public class UserRegistration {
     /**
      * Function for Checking the MobileNo
      * @return True if matches regex pattern otherwise throws exception
-     * @throws ValidationOfException
+     * @throws Exception
      */
     public boolean checkMobileNo(String MobileNo) throws ValidationOfException {
         boolean check = Pattern.matches("^[1-9][0-9][1-9]{10}", MobileNo);
@@ -59,7 +59,7 @@ public class UserRegistration {
     /**
      * Function for Checking the PassWord
      * @return True if matches regex pattern otherwise throws exception
-     * @throws ValidationOfException
+     * @throws Exception
      */
     public boolean checkPassWord(String Password) throws ValidationOfException {
         boolean check = Pattern.matches("^(?=.*[A-Z])(?=.*\\d)(?=.*[a-zA-z0-9!@#$%^&*()_+=-{};':<>,./?~`]).{8,}$",
@@ -70,7 +70,7 @@ public class UserRegistration {
             throw new ValidationOfException("Invalid password");
     }
 
-    public static void main(String[] args) throws ValidationOfException {
+    public static void main(String[] args) throws Exception {
         UserRegistration registration = new UserRegistration();
         registration.checkFirstname("Sru"); // Throws invalid first name exception
         registration.checkLastName("Ma");  //Throws invalid last name exception
